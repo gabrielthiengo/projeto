a2505025d806f6d645ef0bff1805effb61a8aedc
Gabriel Thiengo <gabrielmerces001@hotmail.com>
2020-12-14 11:18:43 -0300

    melhorias

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -15,6 +15,7 @@ import {
   FaCcPaypal,
   FaBarcode,
 } from 'react-icons/fa';
+import Loader from 'react-loader-spinner';
 import { addProductToCart } from '../../store/modules/cart/actions';
 import Navbar from '~/components/Navbar';
 import Product from '~/components/Product';
@@ -31,9 +32,11 @@ function ProductShow() {
   const [color, setColor] = useState('');
   const [imageState, setImageState] = useState('');
   const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
+    setIsLoading(true);
     async function loadProduct() {
       const response = await api.get(`product-show/${id}`);
       setProduct(response.data.product);
@@ -45,6 +48,7 @@ function ProductShow() {
     }
 
     loadProduct();
+    setIsLoading(false);
   }, []);
 
   const handleChangeSize = event => {
@@ -71,103 +75,112 @@ function ProductShow() {
     <div className="wrapper-container">
       <Navbar />
 
-      <main className="product-main w3-animate-opacity">
-        <section className="product-section">
-          <header className="ps-header">
-            <h1>{product.title}</h1>
-          </header>
-          <div className="ps-content">
-            <div className="image-container">
-              <img src={imageState} alt="Foto Produto" />
-              <div className="image-list">
-                <img
-                  src={product.photo_cover}
-                  onClick={() => setImageState(product.photo_cover)}
-                  alt="Foto Produto"
-                />
-                <img
-                  src={product.photo_one}
-                  onClick={() => setImageState(product.photo_one)}
-                  alt="Foto Produto"
-                />
-                <img
-                  src={product.photo_two}
-                  onClick={() => setImageState(product.photo_two)}
-                  alt="Foto Produto"
-                />
-                <img
-                  src={product.photo_three}
-                  onClick={() => setImageState(product.photo_three)}
-                  alt="Foto Produto"
-                />
+      {!isLoading ? (
+        <main className="product-main w3-animate-opacity">
+          <section className="product-section">
+            <header className="ps-header">
+              <h1>{product.title}</h1>
+            </header>
+            <div className="ps-content">
+              <div className="image-container">
+                <img src={imageState} alt="Foto Produto" />
+                <div className="image-list">
+                  <img
+                    src={product.photo_cover}
+                    onClick={() => setImageState(product.photo_cover)}
+                    alt="Foto Produto"
+                  />
+                  <img
+                    src={product.photo_one}
+                    onClick={() => setImageState(product.photo_one)}
+                    alt="Foto Produto"
+                  />
+                  <img
+                    src={product.photo_two}
+                    onClick={() => setImageState(product.photo_two)}
+                    alt="Foto Produto"
+                  />
+                  <img
+                    src={product.photo_three}
+                    onClick={() => setImageState(product.photo_three)}
+                    alt="Foto Produto"
+                  />
+                </div>
+              </div>
+
+              <div className="ps-detail">
+                <h2>Descrição:</h2>
+                <p>{product.full_description}</p>
+
+                <h2>Tamanho:</h2>
+                <Select value={size} onChange={handleChangeSize}>
+                  <MenuItem value="P">P</MenuItem>
+                  <MenuItem value="M">M</MenuItem>
+                  <MenuItem value="G">G</MenuItem>
+                  <MenuItem value="GG">GG</MenuItem>
+                </Select>
+
+                <h2>Cores:</h2>
+                <Select value={color} onChange={handleChangeColor}>
+                  <MenuItem value="PRETO">PRETO</MenuItem>
+                  <MenuItem value="BRANCO">BRANCO</MenuItem>
+                  <MenuItem value="VERMELHO">VERMELHO</MenuItem>
+                  <MenuItem value="AMARELO">AMARELO</MenuItem>
+                </Select>
+
+                <h4>R$ {product.price?.toFixed(2)}</h4>
+
+                <Button type="button" onClick={handleAddProductToCart}>
+                  Adicionar ao Carrinho
+                </Button>
+
+                <div className="address-info">
+                  <input type="text" placeholder="Cep" />
+                  <Button type="button">Calcular</Button>
+                </div>
               </div>
             </div>
+          </section>
 
-            <div className="ps-detail">
-              <h2>Descrição:</h2>
-              <p>{product.full_description}</p>
+          <section className="other-products">
+            <div className="css-line" />
+            <br />
+            <h3>Recomendados para você</h3>
 
-              <h2>Tamanho:</h2>
-              <Select value={size} onChange={handleChangeSize}>
-                <MenuItem value="P">P</MenuItem>
-                <MenuItem value="M">M</MenuItem>
-                <MenuItem value="G">G</MenuItem>
-                <MenuItem value="GG">GG</MenuItem>
-              </Select>
-
-              <h2>Cores:</h2>
-              <Select value={color} onChange={handleChangeColor}>
-                <MenuItem value="PRETO">PRETO</MenuItem>
-                <MenuItem value="BRANCO">BRANCO</MenuItem>
-                <MenuItem value="VERMELHO">VERMELHO</MenuItem>
-                <MenuItem value="AMARELO">AMARELO</MenuItem>
-              </Select>
-
-              <h4>R$ {product.price?.toFixed(2)}</h4>
-
-              <Button type="button" onClick={handleAddProductToCart}>
-                Adicionar ao Carrinho
-              </Button>
-
-              <div className="address-info">
-                <input type="text" placeholder="Cep" />
-                <Button type="button">Calcular</Button>
+            <div className="other-list">
+              {productList.map(productItem => {
+                return <Product key={productItem.id} product={productItem} />;
+              })}
+            </div>
+            <p>
+              *As imagens dos produtos poderão sofrer ligeiras alterações de
+              cores, devido as configurações de seu monitor. Objetos utilizados
+              para ilustração não acompanham o produto.
+            </p>
+          </section>
+          <section className="payments-type">
+            <div className="payments">
+              <h3>FORMAS DE PAGAMENTO</h3>
+              <div className="types">
+                <FaCcVisa size={30} />
+                <FaCcMastercard size={30} />
+                <FaCcPaypal size={30} />
+                <FaBarcode size={30} />
               </div>
             </div>
-          </div>
-        </section>
-
-        <section className="other-products">
-          <div className="css-line" />
-          <br />
-          <h3>Recomendados para você</h3>
-
-          <div className="other-list">
-            {productList.map(productItem => {
-              return <Product key={productItem.id} product={productItem} />;
-            })}
-          </div>
-          <p>
-            *As imagens dos produtos poderão sofrer ligeiras alterações de
-            cores, devido as configurações de seu monitor. Objetos utilizados
-            para ilustração não acompanham o produto.
-          </p>
-        </section>
-        <section className="payments-type">
-          <div className="payments">
-            <h3>FORMAS DE PAGAMENTO</h3>
-            <div className="types">
-              <FaCcVisa size={30} />
-              <FaCcMastercard size={30} />
-              <FaCcPaypal size={30} />
-              <FaBarcode size={30} />
+            <div className="devstore-logo">
+              <h1>DEVSTORE</h1>
             </div>
-          </div>
-          <div className="devstore-logo">
-            <h1>DEVSTORE</h1>
-          </div>
-        </section>
-      </main>
+          </section>
+        </main>
+      ) : (
+        <Loader
+          type="Oval"
+          color="rgb(147, 130, 215)"
+          height={50}
+          width={100}
+        />
+      )}
     </div>
   );
 }
